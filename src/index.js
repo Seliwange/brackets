be90d695c2c3be1257c9f6a87eb9d609f0457cf3
Yuliia Yaremchuk <yul.yaremchuk@gmail.com>
2021-03-15 23:53:15 +0200

    feat: add half solution for task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = "[]{}()||'',,";
+  for(let br of str){
+      let index = brackets.indexOf(br);
+      if(index % 2 === 0){
+          return false;
+      }
+      return true;
+  }
 }
